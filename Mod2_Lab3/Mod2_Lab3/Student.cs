29697ca115b7e93d63a26d8a0c2015923d34a7b3
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-08-20 23:13:02 -0400

    8-20

---
+++ Mod2_Lab3/Mod2_Lab3/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_Lab3
+{
+	public class Student : Person
+	{
+		public Student(string name, int age) : base(name, age)
+		{
+			this.Grade = 75;
+		}
+
+		private static int _studentCount;
+		private int _grade;
+		
+		public int Grade
+		{
+			get
+			{
+				return _grade;
+			}
+			
+			set
+			{
+				_grade = value;
+			}
+		}
+
+		public static int CountStudents()
+		{
+			return _studentCount;
+		}
+	}
+}
